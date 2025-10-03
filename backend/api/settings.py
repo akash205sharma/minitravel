4fc9bfdd8400845c256d91e53b81546c3ab649ee
Akash2005sharma <akashsharma.cpp@gmail.com>
2025-10-03 21:52:46 +0530

    static files python manage.py collectstatic pip freeze > requirements.txt

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&4q421ol#*#(#qi$at2xa70&q13!gk)i_ip3_jwfqw3m+v2oii
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False  
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["minitravel-lac.vercel.app","localhost", "127.0.0.1"]
 
 
 # Application definition
